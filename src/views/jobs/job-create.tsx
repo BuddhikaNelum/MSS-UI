1e419e4dbb2d2e486402fbbfd25d03504432d7ce
Shashika Jayawardhana <shashika.janith@gmail.com>
2023-01-10 22:11:50 +0530

    Implement the jobs feature.

--- src/views/jobs/job-create.tsx
+++ src/views/jobs/job-create.tsx
@@ -1,117 +1,229 @@
-import { Box, Drawer, TextField, Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import {
+  Box,
+  Drawer,
+  TextField,
+  Button,
+  MenuItem,
+  Autocomplete,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 import DrawerHeader from "components/drawer-header";
-import { selectIsCreateDrawerOpen, closeCreateDrawer } from "features/jobs-slice";
+import { selectIsCreateDrawerOpen, closeCreateDrawer, setReload } from "features/jobs-slice";
 import { useAppDispatch, useAppSelector } from "hooks/hooks";
-import { useFormik } from 'formik';
-import * as yup from 'yup';
+import { selectCurrUser, setErrorSnackbar, setSuccessSnackbar } from "features/app-slice";
+import { TJobCreateRequest } from "types/job";
+import { useCreateJobMutation } from "api/jobsAPISlice";
+import { useLazyGetDepartmentsQuery } from "api/departmentsAPISlice";
+import { useLazyGetInventoryQuery } from "api/inventoryAPISlice";
 
 const JobCreate = () => {
   const dispatch = useAppDispatch();
 
+  const [item, setItem] = useState<{ label: string; id: number } | undefined>();
+  const [qty, setQty] = useState(1);
+  const [itemSelected, setItemsSelected] = useState<{ id: number; label: string; qty: number }[]>([]);
+
   const isOpen = useAppSelector(selectIsCreateDrawerOpen);
+  const currUser = useAppSelector(selectCurrUser);
+
+  const [triggerCreateJob] = useCreateJobMutation();
+  const [triggerGetDepartments, { data: departmentList }] = useLazyGetDepartmentsQuery();
+  const [triggerGetInventory, { data: inventoryList }] = useLazyGetInventoryQuery();
+
+  useEffect(() => {
+    triggerGetDepartments();
+    triggerGetInventory();
+  }, []);
 
   const handleClose = () => dispatch(closeCreateDrawer());
 
+  const getMorphedInventoryList = () => {
+    const options: Readonly<{ label: string; id: number }[]> = (inventoryList || []).map((item) => ({
+      label: item.name,
+      id: item.id,
+    }));
+
+    return options;
+  };
+
   const validationSchema = yup.object({
-    email: yup
-      .string()
-      .email('Enter a valid email')
-      .required('Email is required'),
-    phone: yup
-      .string()
-      .required('Phone is required'),
-    firstName: yup
-      .string()
-      .required('Enter your first name'),
-    lastName: yup
-      .string()
-      .required('Enter your last name'),
+    name: yup.string().required("Job name is required"),
+    description: yup.string().required("Job description is required"),
+    departmentId: yup.string().required("Department is required"),
   });
 
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      phone: '',
+      name: "",
+      description: "",
+      departmentId: "1",
     },
     validationSchema: validationSchema,
-    onSubmit: () => { }
+    onSubmit: (values) => {
+      if (itemSelected.length) {
+        const obj: TJobCreateRequest = {
+          name: values.name,
+          description: values.description,
+          departmentId: parseInt(values.departmentId, 10),
+          workMaterials: itemSelected.map((item) => ({ inventoryId: item.id, quantity: item.qty })),
+          userId: currUser!.id,
+        };
+
+        handleCreateJob(obj);
+      }
+    },
   });
 
+  const handleCreateJob = async (values: TJobCreateRequest) => {
+    triggerCreateJob(values)
+      .unwrap()
+      .then((_res) => {
+        dispatch(setSuccessSnackbar("Job created successfully"));
+        dispatch(setReload(true));
+        handleClose();
+      })
+      .catch((err) => {
+        dispatch(setErrorSnackbar(err));
+      });
+  };
+
+  const handleAddToList = () => {
+    if (item && qty) {
+      setItemsSelected([...itemSelected, { ...item, qty: qty }]);
+      setItem(undefined);
+      setQty(1);
+    }
+  };
+
   return (
-    <Drawer
-      anchor={'right'}
-      open={isOpen}
-      onClose={handleClose}
-    >
-      <Box sx={{ width: 500, overflow: 'auto', padding: 3 }}>
-        <DrawerHeader title={"Create Employee"} onClose={handleClose} />
+    <Drawer anchor={"right"} open={isOpen} onClose={handleClose}>
+      <Box sx={{ width: 500, overflow: "auto", padding: 3 }}>
+        <DrawerHeader title={"Create Job"} onClose={handleClose} />
 
         <Box role="presentation">
-
-          <Box display='flex' flexDirection='column' marginBottom={2}>
+          <Box display="flex" flexDirection="column" marginBottom={2}>
             <form onSubmit={formik.handleSubmit}>
-
               <TextField
-                autoComplete="name"
                 margin="normal"
-                name="firstName"
+                name="name"
                 variant="outlined"
                 fullWidth
-                id="firstName"
-                label="First Name"
+                id="name"
+                label="Name"
+                value={formik.values.name}
                 onChange={formik.handleChange}
-                error={formik.touched.firstName && Boolean(formik.errors.firstName)}
-                helperText={formik.touched.firstName && formik.errors.firstName}
+                error={formik.touched.name && Boolean(formik.errors.name)}
+                helperText={formik.touched.name && formik.errors.name}
               />
 
               <TextField
-                autoComplete="name"
-                margin="normal"
-                name="lastName"
-                variant="outlined"
                 fullWidth
-                id="lastName"
-                label="Last Name"
+                multiline
+                maxRows={4}
+                margin="normal"
+                name="description"
+                variant="outlined"
+                id="description"
+                label="Description"
+                value={formik.values.description}
                 onChange={formik.handleChange}
-                error={formik.touched.lastName && Boolean(formik.errors.lastName)}
-                helperText={formik.touched.lastName && formik.errors.lastName}
+                error={formik.touched.description && Boolean(formik.errors.description)}
+                helperText={formik.touched.description && formik.errors.description}
               />
 
               <TextField
+                select
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                autoComplete="email"
+                id="departmentId"
+                label="Department"
+                name="departmentId"
                 onChange={formik.handleChange}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-                helperText={formik.touched.email && formik.errors.email}
-              />
-
-              <TextField
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                id="phone"
-                label="Phone"
-                name="phone"
-                autoComplete="phone"
-                onChange={formik.handleChange}
-                error={formik.touched.phone && Boolean(formik.errors.phone)}
-                helperText={formik.touched.phone && formik.errors.phone}
-              />
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                sx={{ margin: "normal" }}
+                value={formik.values.departmentId}
+                error={formik.touched.departmentId && Boolean(formik.errors.departmentId)}
+                helperText={formik.touched.departmentId && formik.errors.departmentId}
               >
+                {(departmentList || []).map((d) => (
+                  <MenuItem key={d.id} value={d.id}>
+                    {d.name}
+                  </MenuItem>
+                ))}
+              </TextField>
+
+              <Box border={1} padding={1} marginBottom={2} marginTop={2} borderColor="secondary.light" borderRadius={1}>
+                <Box display="flex" alignItems="center" gap={1}>
+                  <Autocomplete
+                    fullWidth
+                    disablePortal
+                    options={getMorphedInventoryList()}
+                    onChange={(_e, v) => {
+                      if (v) {
+                        setItem(v);
+                      }
+                    }}
+                    value={item}
+                    renderInput={(params) => <TextField {...params} margin="none" label="Select item" />}
+                  />
+
+                  <TextField
+                    type="number"
+                    variant="outlined"
+                    margin="normal"
+                    id="quantity"
+                    label="Qty"
+                    name="quantity"
+                    value={qty}
+                    onChange={(e) => setQty(parseInt(e.target.value))}
+                  />
+
+                  <IconButton onClick={handleAddToList}>
+                    <AddIcon />
+                  </IconButton>
+                </Box>
+
+                {itemSelected.length ? (
+                  <Box maxHeight={200}>
+                    <TableContainer>
+                      <Table size="small" stickyHeader>
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell align="right">Qty</TableCell>
+                            <TableCell />
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {itemSelected.map((row) => (
+                            <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                              <TableCell>{row.label}</TableCell>
+                              <TableCell align="right">{row.qty}</TableCell>
+                              <TableCell align="right">
+                                <IconButton aria-label="delete" color="error">
+                                  <DeleteIcon fontSize="small" />
+                                </IconButton>
+                              </TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </Box>
+                ) : undefined}
+              </Box>
+
+              <Button type="submit" fullWidth variant="contained" color="primary" sx={{ margin: "normal" }}>
                 Create
               </Button>
             </form>
@@ -120,6 +232,6 @@ const JobCreate = () => {
       </Box>
     </Drawer>
   );
-}
+};
 
 export default JobCreate;
--- src/views/jobs/job-details.tsx
+++ src/views/jobs/job-details.tsx
@@ -1,62 +1,121 @@
-import { Alert, Box, Drawer, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Drawer,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { useAppDispatch, useAppSelector } from "hooks/hooks";
 import DrawerHeader from "components/drawer-header";
-import RoomCard from 'components/room-card';
-import PaidTwoToneIcon from '@mui/icons-material/PaidTwoTone';
-import { selectIsDetailsDrawerOpen, toggleDetailsDrawer } from "features/jobs-slice";
+import { selectIsDetailsDrawerOpen, setReload, toggleDetailsDrawer } from "features/jobs-slice";
+import { useRequestFromInventoryMutation } from "api/ordersAPISlice";
+import { setErrorSnackbar, setSuccessSnackbar } from "features/app-slice";
+import moment from "moment";
 
 const JobDetails = () => {
   const dispatch = useAppDispatch();
 
-  const isOpen = useAppSelector(selectIsDetailsDrawerOpen);
+  const { isOpen, job } = useAppSelector(selectIsDetailsDrawerOpen);
+
+  const [triggerAcceptJob] = useRequestFromInventoryMutation();
 
   const handleClose = () => dispatch(toggleDetailsDrawer());
 
+  const handleAcceptJob = async () => {
+    triggerAcceptJob({
+      orderdAt: moment().format(),
+      jobId: job!.id,
+    })
+      .unwrap()
+      .then((_res) => {
+        dispatch(setSuccessSnackbar("Job accepted successfully"));
+        dispatch(setReload(true));
+        handleClose();
+      })
+      .catch((_err) => {
+        dispatch(setErrorSnackbar("Operation failed"));
+      });
+  };
+
   return (
-    <Drawer
-      anchor={'right'}
-      open={isOpen}
-      onClose={handleClose}
-    >
-      <Box sx={{ width: 500, overflow: 'auto', padding: 3 }}>
-        <DrawerHeader title={"Reference ID: 11223344"} onClose={handleClose} />
+    <Drawer anchor={"right"} open={isOpen} onClose={handleClose}>
+      <Box sx={{ width: 500, overflow: "auto", padding: 3 }}>
+        <DrawerHeader title={"Job Details"} onClose={handleClose} />
 
-        <Box display='flex' flexDirection='column' gap={2} marginTop={2}>
-          <Alert variant="filled" severity="success" icon={<PaidTwoToneIcon />}>
+        <Box display="flex" flexDirection="column" gap={2} marginTop={2}>
+          {/* <Alert variant="filled" severity="success" icon={<PaidTwoToneIcon />}>
             Paid
-          </Alert>
+          </Alert> */}
 
-          <Stack sx={{ width: '100%' }}>
-            <Typography variant='caption'>Guest</Typography>
-            <Typography variant='body2'>Mr.Shashika Jayawardhana</Typography>
+          <Stack sx={{ width: "100%" }}>
+            <Typography variant="caption">Job Id</Typography>
+            <Typography variant="body2">{`${job?.id}`.padStart(10, "0")}</Typography>
           </Stack>
 
-          <Stack sx={{ width: '100%' }}>
-            <Typography variant='caption'>Email</Typography>
-            <Typography variant='body2'>shashika.j@email.com</Typography>
+          <Stack sx={{ width: "100%" }}>
+            <Typography variant="caption">Job Name</Typography>
+            <Typography variant="body2">{job?.name}</Typography>
           </Stack>
 
-          <Stack sx={{ width: '100%' }}>
-            <Typography variant='caption'>Phone</Typography>
-            <Typography variant='body2'>0770111222</Typography>
+          <Stack sx={{ width: "100%" }}>
+            <Typography variant="caption">Department</Typography>
+            <Typography variant="body2">{job?.department.name}</Typography>
           </Stack>
 
-          <Stack sx={{ width: '100%' }}>
-            <Typography variant='caption'>Check In</Typography>
-            <Typography variant='body2'>25/08/2022</Typography>
+          <Stack sx={{ width: "100%" }}>
+            <Typography variant="caption">Description</Typography>
+            <Typography variant="body2">{job?.description}</Typography>
           </Stack>
 
-          <Stack sx={{ width: '100%' }}>
-            <Typography variant='caption'>Check Out</Typography>
-            <Typography variant='body2'>01/09/2022</Typography>
+          <Stack sx={{ width: "100%" }}>
+            <Typography variant="caption">Created By</Typography>
+            <Typography variant="body2">{job?.applicationUser.email}</Typography>
           </Stack>
 
-          <RoomCard isSelected={false} />
+          <Box maxHeight={200}>
+            <TableContainer>
+              <Table size="small" stickyHeader>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Qty</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {job?.workMaterials.map((row) => (
+                    <TableRow key={row.inventory.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                      <TableCell>{row.inventory.name}</TableCell>
+                      <TableCell align="right">{row.inventory.quantity}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
 
+          <Stack sx={{ width: "100%" }}>
+            <Typography variant="caption">Created At</Typography>
+            <Typography variant="body2">{job?.createdAt}</Typography>
+          </Stack>
+
+          <Stack sx={{ width: "100%" }}>
+            <Typography variant="caption">Completed At</Typography>
+            <Typography variant="body2">{job?.completedAt}</Typography>
+          </Stack>
+
+          <Button fullWidth variant="contained" color="primary" sx={{ margin: "normal" }} onClick={handleAcceptJob}>
+            Accept
+          </Button>
         </Box>
       </Box>
     </Drawer>
   );
-}
+};
 
 export default JobDetails;
