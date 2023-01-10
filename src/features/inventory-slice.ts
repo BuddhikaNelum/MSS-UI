19110be36ed3eb2726fd36b50ac8bf25b5588154
Shashika Jayawardhana <shashika.janith@gmail.com>
2023-01-10 22:10:44 +0530

    Add reducers.

--- src/features/inventory-slice.ts
+++ src/features/inventory-slice.ts
@@ -1,6 +1,6 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from 'store/store';
-import { TInventory } from 'types/inventory';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "store/store";
+import { TInventory } from "types/inventory";
 
 interface IState {
   reload: boolean;
@@ -17,16 +17,13 @@ const initialState: IState = {
 };
 
 export const inventorySlice = createSlice({
-  name: 'inventory',
+  name: "inventory",
   initialState,
   reducers: {
     setReload: (state: IState, action: PayloadAction<boolean>) => {
       state.reload = action.payload;
     },
-    openCreateDrawer: (
-      state: IState,
-      action: PayloadAction<TInventory | undefined>
-    ) => {
+    openCreateDrawer: (state: IState) => {
       state.isOpenCreateDrawer = true;
     },
     closeCreateDrawer: (state: IState) => {
@@ -38,18 +35,10 @@ export const inventorySlice = createSlice({
   },
 });
 
-export const {
-  setReload,
-  openCreateDrawer,
-  closeCreateDrawer,
-  toggleDetailsDrawer,
-} = inventorySlice.actions;
+export const { setReload, openCreateDrawer, closeCreateDrawer, toggleDetailsDrawer } = inventorySlice.actions;
 
-export const selectShouldReload = (state: RootState) =>
-  state.departments.reload;
-export const selectIsCreateDrawerOpen = (state: RootState) =>
-  state.departments.isOpenCreateDrawer;
-export const selectIsDetailsDrawerOpen = (state: RootState) =>
-  state.departments.isOpenDetailsDrawer;
+export const selectShouldReload = (state: RootState) => state.inventory.reload;
+export const selectIsCreateDrawerOpen = (state: RootState) => state.inventory.isOpenCreateDrawer;
+export const selectIsDetailsDrawerOpen = (state: RootState) => state.inventory.isOpenDetailsDrawer;
 
 export default inventorySlice.reducer;
